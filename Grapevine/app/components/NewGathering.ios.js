afe86dbee2a2d90f0a83703a5fee15de136a5f25
Misha Ponizil <misha.ponizil@gmail.com>
2015-11-15 23:57:15 -0500

    Forgot to remove NewGathering debug code

--- Grapevine/app/components/NewGathering.ios.js
+++ Grapevine/app/components/NewGathering.ios.js
@@ -135,21 +135,10 @@ class NewGathering extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: "test",
-      inviteList: [{
-        id: "7-7608463179",
-        label: "Misha Ponizil",
-        phoneNumber: "7608463179"
-      }],
-      location: {
-        coordinates: {
-          latitude: 40.737042,
-          longitude: -73.990921
-        },
-        name: "Republic",
-        title: "37 Union Sq W, Unit 1, New York, NY  10003, United States"
-      },
-      date: new Date(),
+      title: "",
+      inviteList: [],
+      location: null,
+      date: null,
       initialMessage: "",
       showContactsChooserHelp: false,
       showLocationPicker: false,
